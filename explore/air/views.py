ae63e17c72d8543989e45d7e8f800f0e9de5313a
Greg Vargas <friendofrobots@gmail.com>
2010-11-30 22:23:50 -0500

    updating with new backend

--- explore/air/views.py
+++ explore/air/views.py
@@ -5,50 +5,50 @@ from django.template import RequestContext
 
 from air.models import FBGraph
 import pickle
-from explore.air.divisi import divisi_fb
+from explore.air.divisi import airtoolkit
 
 # Choose a Profile/Page or Categories
 def explore(request, template_name="explore/explore.html"):
-    divisifb = divisi_fb.DivisiFB(FBGraph.objects.all()[0].filename)
+    toolkit = airtoolkit.AIRToolkit(FBGraph.objects.all()[0].filename)
     return render_to_response(template_name, {
-        "profiles": divisifb.getProfiles(),
-        "pages": divisifb.getPages(),
+        "profiles": toolkit.getType('profile'),
+        "pages": toolkit.getType('page'),
         }, context_instance=RequestContext(request))
 
 # Given profile, choose action
 def object(request, id, template_name="explore/object.html"):
-    divisifb = divisi_fb.DivisiFB(FBGraph.objects.all()[0].filename)
-    fbobject = divisifb.getById(id)
+    toolkit = airtoolkit.AIRToolkit(FBGraph.objects.all()[0].filename)
+    fbobject = toolkit.getById(id)
     return render_to_response(template_name, {
         "object": fbobject,
-        "topSim": divisifb.topSimilarity(id),
-        "topPre": divisifb.topPredictions(id),
+        "topSim": toolkit.topSimilarity(id),
+        "topPre": toolkit.topPredictions(id),
         }, context_instance=RequestContext(request))
 
 # Choose what to compare it to
 def compareTo(request, id, template_name="explore/compareto.html"):
-    divisifb = divisi_fb.DivisiFB(FBGraph.objects.all()[0].filename)
+    toolkit = airtoolkit.AIRToolkit(FBGraph.objects.all()[0].filename)
     return render_to_response(template_name, {
-        "object": divisifb.getById(id),
-        "profiles": divisifb.getProfiles(),
-        "pages": divisifb.getPages(),
+        "object": toolkit.getById(id),
+        "profiles": toolkit.getType('profile'),
+        "pages": toolkit.getType('page'),
         }, context_instance=RequestContext(request))
 
 # Calculate similarity
 def compare(request, id1, id2, template_name="explore/compare.html"):
-    divisifb = divisi_fb.DivisiFB(FBGraph.objects.all()[0].filename)
+    toolkit = airtoolkit.AIRToolkit(FBGraph.objects.all()[0].filename)
     return render_to_response(template_name, {
-        "object1": divisifb.getById(id1),
-        "object2": divisifb.getById(id2),
-        "weight": divisifb.compare(id1,id2),
+        "object1": toolkit.getById(id1),
+        "object2": toolkit.getById(id2),
+        "weight": toolkit.compare(id1,id2),
         }, context_instance=RequestContext(request))
 
 # Choose items for category
 def categories(request, template_name="explore/categories.html"):
-    divisifb = divisi_fb.DivisiFB(FBGraph.objects.all()[0].filename)
+    toolkit = airtoolkit.AIRToolkit(FBGraph.objects.all()[0].filename)
     return render_to_response(template_name, {
-        "profiles": divisifb.getProfiles(),
-        "pages": divisifb.getPages(),
+        "profiles": toolkit.getType('profile'),
+        "pages": toolkit.getType('pages'),
         }, context_instance=RequestContext(request))
 
 # Show category features
@@ -58,36 +58,45 @@ def category(request, template_name="explore/category.html"):
     ids = request.POST.getlist('object')
     if len(ids) < 1:
         return HttpResponseRedirect(reverse('categories'))
-    divisifb = divisi_fb.DivisiFB(FBGraph.objects.all()[0].filename)
-    category = divisifb.createCategory(ids)
+    toolkit = airtoolkit.AIRToolkit(FBGraph.objects.all()[0].filename)
+    category = toolkit.createCategory(ids)
     return render_to_response(template_name, {
-        "objects": [divisifb.getById(id) for id in ids],
-        "topFea": divisifb.categoryTopFeatures(category),
-        "topSim": divisifb.categoryTopSimilarity(category),
-        "topPre": divisifb.categoryTopPredictions(category),
+        "objects": [toolkit.getById(id) for id in ids],
+        "topFea": toolkit.categoryTopFeatures(category),
+        "topSim": toolkit.categoryTopSimilarity(category),
+        "topPre": toolkit.categoryTopPredictions(category),
         }, context_instance=RequestContext(request))
 
 def projection(request, id1, id2, thresh=.01, template_name="explore/projection.html"):
-    divisifb = divisi_fb.DivisiFB(FBGraph.objects.all()[0].filename)
+    toolkit = airtoolkit.AIRToolkit(FBGraph.objects.all()[0].filename)
     return render_to_response(template_name, {
-        "profile1": divisifb.getById(id1),
-        "profile2": divisifb.getById(id2),
-        "projected_likes": divisifb.project_prediction(id1,id2,thresh=thresh),
+        "profile1": toolkit.getById(id1),
+        "profile2": toolkit.getById(id2),
+        "projected_likes": toolkit.project_prediction(id1,id2,thresh=thresh),
         }, context_instance=RequestContext(request))
 
-def add(request, template_name="explore/projection.html"):
-    divisifb = divisi_fb.DivisiFB(FBGraph.objects.all()[0].filename)
+def add(request, template_name="explore/add.html"):
+    toolkit = airtoolkit.AIRToolkit(FBGraph.objects.all()[0].filename)
     if request.method == 'POST':
-        data = {}
-        data['name'] = request.POST.get('name')
-        data['gender'] = request.POST.get('gender')
-        data['hometown'] = request.POST.get('hometown')
-        likeids = request.POST.getlist('likes')
-        profile = divisifb.createProfile("FBGraphAltered",data,likeids)
-        FBGraph.objects.all()[0].filename = "FBGraphAltered.pickle"
-        FBGraph.objects.all()[0].altered = True
+        keylinks = [('gender',(request.POST.get('gender'),'hasProperty',1)),
+                ('hometown',(request.POST.get('hometown'),'isFrom',1))]
+        keylinks.extend([('like',(likeid,'likes',1)) for likeid in request.POST.getlist('likes')])
+        profileName = request.POST.get('name')
+        profile = toolkit.createProfile("FBGraphAltered",profileName,keylinks)
+        graphpointer = FBGraph.objects.all()[0]
+        graphpointer.filename = "FBGraphAltered.pickle"
+        graphpointer.altered = True
+        graphpointer.save()
         return HttpResponseRedirect(reverse('object',args=[profile.id]))
     return render_to_response(template_name, {
-        "hometowns": [x for x in divisifb.getPages() if x.category == 'City']
-        "likes": [x for x in divisifb.getPages() if x.category != 'Gender']
+        "hometowns": [x for x in toolkit.getType('page') if x.getLink('category') == 'City'],
+        "likes": [x for x in toolkit.getType('page') if x.getLink('category') != 'Gender'
+                  and x.getLink('category') != 'Category'],
         }, context_instance=RequestContext(request))
+
+def reset(request):
+    graphpointer = FBGraph.objects.all()[0]
+    graphpointer.filename = "FBGraph.pickle"
+    graphpointer.altered = False
+    graphpointer.save()
+    return HttpResponseRedirect(reverse('explore'))
