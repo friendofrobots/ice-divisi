7bb6f5b3a9725696424d20aa6bb6f1088a1d5521
Greg Vargas <friendofrobots@gmail.com>
2010-11-30 23:33:59 -0500

    made media directory smart

--- explore/urls.py
+++ explore/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+import os.path
 admin.autodiscover()
+MEDIA_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), "media")
 
 urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
 
     (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-     {'document_root': '/Users/friendofrobots/code/ice-divisi/explore/media'}),
+     {'document_root': MEDIA_ROOT}),
     url(r'^p/(\w+)$', 'air.views.object', name='object'),
     url(r'^compare/(\w+)$', 'air.views.compareTo', name='compareTo'),
     url(r'^compare/(\w+)/(\w+)$', 'air.views.compare', name='compare'),
